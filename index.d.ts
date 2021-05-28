9b31e4d61483b5ea23b5a77807e7e909108035a9
Jesse Bangs <jaspax@gmail.com>
2021-05-28 20:50:08 +0300

    extend src types as they should be

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,42 @@
-import * as React from 'react';
+import * as React from "react";
+
+type SourceType = string | Function | React.ReactNode | React.ElementType;
 
 export interface ILightBoxProps {
-    mainSrc: string;
-    nextSrc?: string;
-    prevSrc?: string;
-    mainSrcThumbnail?: string;
-    prevSrcThumbnail?: string;
-    nextSrcThumbnail?: string;
-    onCloseRequest(): void;
-    onMovePrevRequest?(): void;
-    onMoveNextRequest?(): void;
-    onImageLoad?(): void;
-    onImageLoadError?(): void;
-    imageLoadErrorMessage?: React.ReactNode;
-    onAfterOpen?(): void;
-    discourageDownloads?: boolean;
-    animationDisabled?: boolean;
-    animationOnKeyInput?: boolean;
-    animationDuration?: number;
-    keyRepeatLimit?: number;
-    keyRepeatKeyupBonus?: number;
-    imageTitle?: string;
-    imageCaption?: string;
-    imageCrossOrigin?: string;
-    toolbarButtons?: React.ReactNode[];
-    reactModalStyle?: any;
-    reactModalProps?: any;
-    imagePadding?: number;
-    clickOutsideToClose?: boolean;
-    enableZoom?: boolean;
-    wrapperClassName?: string;
-    nextLabel?: string;
-    prevLabel?: string;
-    zoomInLabel?: string;
-    zoomOutLabel?: string;
-    closeLabel?: string;
+  mainSrc: SourceType;
+  nextSrc?: SourceType;
+  prevSrc?: SourceType;
+  mainSrcThumbnail?: string;
+  prevSrcThumbnail?: string;
+  nextSrcThumbnail?: string;
+  onCloseRequest(): void;
+  onMovePrevRequest?(): void;
+  onMoveNextRequest?(): void;
+  onImageLoad?(): void;
+  onImageLoadError?(): void;
+  imageLoadErrorMessage?: React.ReactNode;
+  onAfterOpen?(): void;
+  discourageDownloads?: boolean;
+  animationDisabled?: boolean;
+  animationOnKeyInput?: boolean;
+  animationDuration?: number;
+  keyRepeatLimit?: number;
+  keyRepeatKeyupBonus?: number;
+  imageTitle?: string;
+  imageCaption?: string;
+  imageCrossOrigin?: string;
+  toolbarButtons?: React.ReactNode[];
+  reactModalStyle?: any;
+  reactModalProps?: any;
+  imagePadding?: number;
+  clickOutsideToClose?: boolean;
+  enableZoom?: boolean;
+  wrapperClassName?: string;
+  nextLabel?: string;
+  prevLabel?: string;
+  zoomInLabel?: string;
+  zoomOutLabel?: string;
+  closeLabel?: string;
 }
 
-export default class Lightbox extends React.Component<ILightBoxProps, never> { }
+export default class Lightbox extends React.Component<ILightBoxProps, never> {}
